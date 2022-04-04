6c3f284c854a94db2fbf9830719a9eac68e058a0
Jack Dragoon <deulamco@iMac-deulamco.local>
2022-04-04 11:24:19 +0700

    add crypto-utils manually

---
+++ wallet.ts
@@ -0,0 +1,55 @@
+///
+import { utils, hd } from './crypto-utils'
+import { TezosToolkit } from '@taquito/taquito'
+import { InMemorySigner } from '@taquito/signer'
+///
+const balanceDiv = 1000000 // div by 1M
+/// 
+let rpcUrl = "https://ithacanet.ecadinfra.com"  // Ithaca Testnet.
+let tezos = new TezosToolkit(rpcUrl)            // init new instance.
+//
+// const mnemonic_sample = [ //TODO: find out mnemonic rules to generate same seed.
+//   "run", "include", "giggle", "half", "dizzy", "worth", "broccoli", "faith", 
+//   "current", "wheel", "depth", "juice", "reduce", "width", "doctor"
+// ]
+// mnemonic_sample.join(' ').toLowerCase().replace(/(\r\n|\n|\r)/gm, ' ').trim()
+///
+let mnemonic = 'run include giggle half dizzy worth broccoli faith current wheel depth juice reduce width doctor'
+///
+///
+let keyPair = hd.keyPairFromAccountIndex(utils.mnemonicToSeed(mnemonic, '', true), 1)
+// console.log("pkh: %s ", keyPair.pkh)
+const signer = new InMemorySigner(keyPair.sk || '')
+tezos.setSignerProvider(signer)
+/// correct address :
+const address = "tz1hYe3pVtPq8JprqjFCSxrhpbfHPwDYVLXX"
+
+const main = async () => {
+  const account = await signer.publicKeyHash() 
+  let balance = (await tezos.rpc.getBalance(account)).toNumber()
+  console.log("wallet: %s \nbalance: %d tez\n", account, balance/balanceDiv)
+  /// invoke test Tx:
+  await transfer_to(account, address , 0.1)//tez
+}
+
+main()
+///
+/// @description: Transfer to test wallet on testnet
+async function transfer_to(account: string, address: string, amount: number)
+{
+  let counting = new Date().getTime()
+  console.log("sending %d tez >> %s", amount, address)
+  let op = await tezos.wallet.transfer({
+    to: address,
+    amount: amount
+  }).send()
+  let result = await op.confirmation() // listen for confirmation 
+  ///
+  if(result.completed) {
+    let duration = (new Date().getTime() - counting) % (1000 * 60) / 1000
+    // refresh balance again.
+    let balance = (await tezos.rpc.getBalance(account)).toNumber()/balanceDiv 
+    console.log("%ds for Tx: %s \nbalance: %d tez\n" ,duration ,result.block.hash, balance)
+  } 
+  else console.log("error: %s", result.block)  
+}
